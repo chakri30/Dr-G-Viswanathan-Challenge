27345037282e386777ddbb6847b8e574b73b03f1
Ch.Chakri <chakrichindiri2022@gmail.com>
2025-12-17 15:04:34 +0530

    Add files via upload

---
+++ Day3/Transverse of a Matrix/TransposeMatrix.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        int[][] result = transpose(matrix);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.println(result[i][j] + "");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] transpose(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] transpose = new int[col][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        return transpose;
+    }
+}
